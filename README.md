479bb095b68316344fefa8fd6082994af0675518
WooYeong Choe <whoo24@gmail.com>
2018-11-01 23:38:10 +0900

    add glm library

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sdl2-glew
 
-PS> Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri 'https://libsdl.org/release/SDL2-devel-2.0.8-VC.zip' -OutFile '.\SDL2-devel-2.0.8-VC.zip'
+``` PS> Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri 'https://libsdl.org/release/SDL2-devel-2.0.8-VC.zip' -OutFile '.\SDL2-devel-2.0.8-VC.zip' ```
 
 https://sourceforge.net/projects/glew/files/glew/2.1.0/glew-2.1.0-win32.zip/download
+
+https://github.com/g-truc/glm/releases/download/0.9.9.3/glm-0.9.9.3.zip
--- Src/SDL2GlewMain/SDL2GlewMain.cpp
+++ Src/SDL2GlewMain/SDL2GlewMain.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 
 #include "SDL2GlewMain/Application.h"
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
 
 enum class EventResult {
   kUpdate,
@@ -26,24 +28,38 @@ EventResult UpdateEventHandler() {
   return EventResult::kUpdate;
 }
 
-void Render(SDL_Window* window) {
+struct RenderContext {
+  float fov = 45.0f;
+  float width = 512.f;
+  float height = 512.f;
+  float near_plane = 0.01f;
+  float far_plane = 10000.f;
+  glm::vec3 eye = { 0, 0, 10.f };
+  glm::vec3 look_at = { 0, 0, 0 };
+  glm::vec3 up = { 0, 1.f, 0 };
+};
+
+void Render(SDL_Window* window, RenderContext* render) {
+  glm::mat4 mat_proj = glm::perspective(render->fov, render->width / render->height, render->near_plane, render->far_plane);
+  glm::mat4 mat_view = glm::lookAt(render->eye, render->look_at, render->up);
   glClearColor(0.5, 0.5, 0.5, 1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   SDL_GL_SwapWindow(window);
 }
 
-void Update(AppContext* app) {
+void Update(AppContext* app, RenderContext* render) {
   auto event = EventResult::kUpdate;
   while (event != EventResult::kQuit) {
     event = UpdateEventHandler();
-    Render(app->main_window);
+    Render(app->main_window, render);
   }
 }
 
 int main(int argc, char *argv[]) {
   AppContext context;
+  RenderContext render_context;
   Init(&context);
-  Update(&context);
+  Update(&context, &render_context);
   CleanUp(&context);
   return 0;
 }
